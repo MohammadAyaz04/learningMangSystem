9ae95758e9b3e1a00396227ec3aaea1fb1a52da7
Mohammad Ayaz <mohdayaz7068@gmail.com>
2026-06-03 23:11:51 +0530

    small

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -5,13 +5,13 @@ export const clerkWebhooks = async (req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
         
-        await whook.verify(req.body, {
+        await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
         })
 
-        const { data, type } = JSON.parse(req.body)
+        const { data, type } = req.body
 
         switch (type) {
             case "user.created": {
